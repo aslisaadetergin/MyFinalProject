3bd88f104d70c55d7449a1fb6258cf441ee92ab4
aslı saadet ergin <erginsaadet03@gmail.com>
2021-02-14 00:10:04 +0300

    13 Şubat ders sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -37,7 +37,7 @@ namespace Business.Concrete
         {
             // İş kodları
             //Yetkisi var mı? varsa ürünleri listele
-            if (DateTime.Now.Hour == 22)
+            if (DateTime.Now.Hour == 1)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
